24cbae885c7c6e49bf9c09e8d30b64ee8e48bc08
hoouda19 <hoouda19@gmail.com>
2023-03-07 13:55:35 +0200

    7/3

--- lib/view/routes/onboard_page_view.dart
+++ lib/view/routes/onboard_page_view.dart
@@ -8,11 +8,11 @@ class onboardPageView extends StatelessWidget {
 
   List<Onboardmodel> onBoradList = [
     Onboardmodel('Resturant App', 'Welcom To Resturant App',
-        'assets/images/onBoard/onboard1.png', '', true),
+        'asset/images/onb1.png', '', true),
     // Onboardmodel('Coffee App', 'In This App You Can Buy Any Coffee',
     //     'assets/onboardimage/pngwing1.png'),
-    Onboardmodel('Resturant App', 'The Best Price',
-        'assets/images/onBoard/onboard.png', 'START', false)
+    Onboardmodel('Resturant App', 'The Best Price', 'asset/images/onb1.png',
+        'START', false)
   ];
   static final contrpage = PageController();
 
